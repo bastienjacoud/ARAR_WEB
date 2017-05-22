c63d311b44db51c72cf1d760e1e23ff310a5bcc1
BastienJacoud <bastien.jacoud@etu.univ-lyon1.fr>
2017-05-22 08:27:12 +0200

    .

--- projet_web/src/projet_web/ServeurWeb.java
+++ projet_web/src/projet_web/ServeurWeb.java
@@ -2,16 +2,19 @@ package projet_web;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServeurWeb {
 
 	ServerSocket ss;
+	Socket con_cli;
 
 	public ServeurWeb()
 	{
 		try
 		{
 			ss = new ServerSocket(1234);
+			con_cli = new Socket();
 		}
 		catch (IOException e)
 		{
