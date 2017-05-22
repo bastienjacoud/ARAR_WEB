b6c2f222918d6baca727e4b5fc8835f870851d57
BastienJacoud <bastien.jacoud@etu.univ-lyon1.fr>
2017-05-22 08:25:45 +0200

    .

--- projet_web/src/projet_web/ServeurWeb.java
+++ projet_web/src/projet_web/ServeurWeb.java
@@ -1,5 +1,23 @@
 package projet_web;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ServeurWeb {
+
+	ServerSocket ss;
+
+	public ServeurWeb()
+	{
+		try
+		{
+			ss = new ServerSocket(1234);
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
